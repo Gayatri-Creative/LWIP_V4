def5d8410df1b62ac45d4664353b2fe0055d877e
Gayatri-Creative <gayatrilendave@mirafra.com>
2025-09-01 14:12:20 +0530

    Added LWIP/IPV4 Stack to main branch

---
+++ TriCore Debug (TASKING)/LogBuffer.d
@@ -0,0 +1,120 @@
+LogBuffer.o :	../LogBuffer.c
+../LogBuffer.c :
+LogBuffer.o :	..\ringbuf.h
+..\ringbuf.h :
+LogBuffer.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h" :
+LogBuffer.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stdint.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stdint.h" :
+LogBuffer.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\typeinfo.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\typeinfo.h" :
+LogBuffer.o :	..\LogBuffer.h
+..\LogBuffer.h :
+LogBuffer.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stdarg.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stdarg.h" :
+LogBuffer.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stdio.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stdio.h" :
+LogBuffer.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\string.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\string.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\\Cpu\\Std\Ifx_Types.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\\Cpu\\Std\Ifx_Types.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Platform\Tricore\Compilers\Compilers.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Platform\Tricore\Compilers\Compilers.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Configurations\Ifx_Cfg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Configurations\Ifx_Cfg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Platform\Tricore\Compilers\CompilerTasking.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Platform\Tricore\Compilers\CompilerTasking.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\\Cpu\\Std\Platform_Types.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\\Cpu\\Std\Platform_Types.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\\Cpu\\Std\Ifx_TypesTasking.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\\Cpu\\Std\Ifx_TypesTasking.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\\Asclin\\Asc\IfxAsclin_Asc.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\\Asclin\\Asc\IfxAsclin_Asc.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Asclin\Std\IfxAsclin.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Asclin\Std\IfxAsclin.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_Impl\IfxAsclin_cfg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_Impl\IfxAsclin_cfg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Cpu\Std\Ifx_Types.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Cpu\Std\Ifx_Types.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxAsclin_reg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxAsclin_reg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxAsclin_regdef.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxAsclin_regdef.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\Ifx_TypesReg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\Ifx_TypesReg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Scu\Std\IfxScuCcu.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Scu\Std\IfxScuCcu.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_Impl\IfxScu_cfg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_Impl\IfxScu_cfg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxScu_bf.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxScu_bf.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Scu\Std\IfxScuWdt.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Scu\Std\IfxScuWdt.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxScu_reg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxScu_reg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxScu_regdef.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxScu_regdef.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Scu\Std\IfxScuWdt.asm.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Scu\Std\IfxScuWdt.asm.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Cpu\Std\IfxCpu_Intrinsics.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Cpu\Std\IfxCpu_Intrinsics.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Cpu\Std\IfxCpu_IntrinsicsTasking.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Cpu\Std\IfxCpu_IntrinsicsTasking.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Cpu\Std\IfxCpu.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Cpu\Std\IfxCpu.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_Impl\IfxCpu_cfg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_Impl\IfxCpu_cfg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxCpu_reg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxCpu_reg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxCpu_regdef.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxCpu_regdef.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxSrc_reg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxSrc_reg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxSrc_regdef.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxSrc_regdef.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxStm_reg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxStm_reg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxStm_regdef.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxStm_regdef.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Service\\CpuGeneric\_Utilities\Ifx_Assert.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Service\\CpuGeneric\_Utilities\Ifx_Assert.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Scu\Std\IfxScuCcu.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Scu\Std\IfxScuCcu.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_PinMap\IfxScu_PinMap.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_PinMap\IfxScu_PinMap.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Port\Std\IfxPort.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Port\Std\IfxPort.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_Impl\IfxPort_cfg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_Impl\IfxPort_cfg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxPort_reg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxPort_reg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxPort_regdef.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxPort_regdef.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxSmu_reg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxSmu_reg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxSmu_regdef.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxSmu_regdef.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxDmu_reg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxDmu_reg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxDmu_regdef.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxDmu_regdef.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Src\Std\IfxSrc.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Src\Std\IfxSrc.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_Impl\IfxSrc_cfg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_Impl\IfxSrc_cfg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_PinMap\IfxAsclin_PinMap.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_PinMap\IfxAsclin_PinMap.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_Lib\DataHandling\Ifx_Fifo.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_Lib\DataHandling\Ifx_Fifo.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Stm\Std\IfxStm.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\Stm\Std\IfxStm.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_Impl\IfxStm_cfg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\iLLD\\TC39B\\Tricore\_Impl\IfxStm_cfg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxCbs_reg.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxCbs_reg.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxCbs_regdef.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Infra\\Sfr\\TC39B\\_Reg\IfxCbs_regdef.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Service\\CpuGeneric\StdIf\IfxStdIf_DPipe.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Service\\CpuGeneric\StdIf\IfxStdIf_DPipe.h" :
+LogBuffer.o :	"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Service\\CpuGeneric\StdIf\IfxStdIf.h"
+"C:\\Users\\admin\\AURIX-v1.10.6-workspace\\Ethernet_1_KIT_TC397_TFT\\Libraries\\Service\\CpuGeneric\StdIf\IfxStdIf.h" :
---
+++ TriCore Debug (TASKING)/ringbuf.d
@@ -0,0 +1,20 @@
+ringbuf.o :	../ringbuf.c
+../ringbuf.c :
+ringbuf.o :	..\ringbuf.h
+..\ringbuf.h :
+ringbuf.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h" :
+ringbuf.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stdint.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stdint.h" :
+ringbuf.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\typeinfo.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\typeinfo.h" :
+ringbuf.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stdlib.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stdlib.h" :
+ringbuf.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\string.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\string.h" :
+ringbuf.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\assert.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\assert.h" :
+ringbuf.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stdio.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stdio.h" :
+ringbuf.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stdarg.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stdarg.h" :
